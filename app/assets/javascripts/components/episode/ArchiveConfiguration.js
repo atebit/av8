c67577ca16200a4881e38bfe07d8476522297840
atebit <chris@adjustcreative.com>
2016-08-10 18:38:14 -0500

    setup api controller to handle episode states and calls

---
+++ app/assets/javascripts/components/episode/ArchiveConfiguration.js
@@ -0,0 +1,31 @@
+var ArchiveConfiguration = {
+
+  /*
+  TRACK CONFIG
+  
+    tracks: [
+      {
+        id: 1,
+        stream_id: guid,
+        user_email: user_email
+
+        assets: [
+          {
+            start_time: UTC Start Time,
+            end_time: UTC End Time,
+            screen_position: {
+              describe how to place it here.
+            }
+            file: {
+              type: video
+              name: blablabla.mp4
+            }
+          }
+        ]
+      }
+    ]
+
+  */
+
+
+};
--- app/controllers/api/episodes_controller.rb
+++ app/controllers/api/episodes_controller.rb
@@ -1,16 +1,40 @@
 class Api::EpisodesController < Api::ApiController
-
-  require 'pusher'
   
   before_action :authenticate_user!
 
-  def update
+
+  # TODO: add fields to model
+  # field: episode start time
+  # field: episode end time
+  # field: episode_state
+  # field: archive_compile_config
+
+
+  def set_episode_state
     @episode = Episode.find( params[:episode_id] )
+    # set episode "session" state
+    # PRE_SHOW / LIVE / ENDED
+  end
 
-    # binding.pry
-    Pusher.trigger(@episode.remote_session_id.to_s, 'update-view', {:message => params[:message].to_json})
-    render json: "1"
+  def get_episode_state
+    # just return the session state
+    # PRE_SHOW / LIVE / ENDED
+  end
 
+  def start_episode
+    @episode = Episode.find( params[:episode_id] )
+    # start archiving
+  end
+
+  def end_episode
+    @episode = Episode.find( params[:episode_id] )
+    # save end time
+    # start compile and archive processs??
+  end
+
+  def update_archive_compile_config
+    # a function for updating the configuration 
+    # for use by ffmpeg to compile assets later
   end
 
 
